3f502b6e805bfa62cda7610761dcf2d704777bc7
Ivan Guerreschi <ivanguerreschi86@gmail.com>
2023-11-20 14:39:41 +0100

    Parse input using clap

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,53 @@
 // Copyright (c) 2023 Ivan Guerreschi. All rights reserved.
 // Licensed under the MIT License. See LICENSE in the project root for license information.
 
-use std::env;
-use std::io;
+use clap::{Args, Parser, Subcommand};
 
 use crabpwd::csv::{delete, new, print_all, search, Password};
 use crabpwd::file::{create, open};
 
-fn help() {
-    println!(
-        "
-        crabpwd Password manager\n
-        a    Print all password
-        s    <String>  Search password
-        d    <Int>     Delete password    
-        n    Create new password
-        "
-    );
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    /// All password
+    All(AllArgs),
+    /// Delete password
+    Delete(DeleteArgs),
+    /// New password
+    New(NewArgs),
+    /// Search password
+    Search(SearchArgs),
+}
+
+#[derive(Args)]
+struct AllArgs {
+    #[arg(default_value_t = 0)]
+    number: usize,
+}
+
+#[derive(Args)]
+struct DeleteArgs {
+    number: usize,
+}
+
+#[derive(Args)]
+struct NewArgs {
+    website: Option<String>,
+    username: Option<String>,
+    email: Option<String>,
+    pwd: Option<String>,
+}
+
+#[derive(Args)]
+struct SearchArgs {
+    key: Option<String>,
 }
 
 fn main() {
@@ -26,97 +57,72 @@ fn main() {
         Err(error) => eprintln!("Error create file {:?}", error),
     }
 
-    let args: Vec<String> = env::args().collect();
+    let cli = Cli::parse();
 
-    help();
-
-    match args.len() {
-        1 => {
-            println!("No arguments passed");
-        }
-
-        2 => {
-            let cmd = &args[1];
-            if cmd == "a" {
-                let open_file = open();
-                match open_file {
-                    Ok(file) => {
-                        print_all(file);
-                    }
-                    Err(error) => eprintln!("Error print all password: {:?}", error),
+    match &cli.command {
+        Commands::All(_number) => {
+            let open_file = open();
+            match open_file {
+                Ok(file) => {
+                    print_all(file);
                 }
-            } else if cmd == "n" {
-                let mut website = String::new();
-                let mut username = String::new();
-                let mut email = String::new();
-                let mut pwd = String::new();
+                Err(error) => eprintln!("Error print all password: {:?}", error),
+            }
+        }
+        Commands::Delete(number) => {
+            let result_delete = delete(number.number);
+            match result_delete {
+                Ok(_) => println!("Delete password in position {}", number.number),
+                Err(error) => {
+                    eprintln!(
+                        "Error to delete password in position {} {}",
+                        number.number, error
+                    )
+                }
+            }
+        }
+        Commands::New(password) => match password.website {
+            Some(ref _password) => {
+                let website = _password.to_owned();
 
-                println!("Website");
-                io::stdin()
-                    .read_line(&mut website)
-                    .expect("Failed to read line");
-                website = website.trim().to_string();
-
-                println!("Username");
-                io::stdin()
-                    .read_line(&mut username)
-                    .expect("Failed to read line");
-                username = username.trim().to_string();
-
-                println!("Email");
-                io::stdin()
-                    .read_line(&mut email)
-                    .expect("Failed to read line");
-                email = email.trim().to_string();
-
-                println!("Password");
-                io::stdin()
-                    .read_line(&mut pwd)
-                    .expect("Failed to read line");
-                pwd = pwd.trim().to_string();
-
-                let new_password = Password {
-                    website: &website,
-                    username: &username,
-                    email: &email,
-                    pwd: &pwd,
+                let username = match password.username {
+                    Some(ref _username) => _username.to_owned(),
+                    None => "*".to_string(),
                 };
 
+                let email = match password.email {
+                    Some(ref _email) => _email.to_owned(),
+                    None => "*".to_string(),
+                };
+
+                let pwd = match password.pwd {
+                    Some(ref _pwd) => _pwd.to_owned(),
+                    None => "*".to_string(),
+                };
+
+                let new_password = Password {
+                    website: website.as_str(),
+                    username: username.as_str(),
+                    email: email.as_str(),
+                    pwd: pwd.as_str(),
+                };
                 let create_password = new(new_password);
                 match create_password {
                     Ok(_) => println!("Password create"),
                     Err(error) => eprintln!("Error new password not create {:?}", error),
                 }
-            } else {
-                eprintln!("error: invalid command");
-                help();
             }
-        }
-
-        3 => {
-            let cmd = &args[1];
-            let key = &args[2];
-
-            if cmd == "s" {
-                let result_search = search(key);
+            None => eprintln!("Insert correct value for new password"),
+        },
+        Commands::Search(key) => match key.key {
+            Some(ref _key) => {
+                let result_search = search(_key);
                 match result_search {
                     Some(search) => println!("{}", search),
                     None => println!("Not found"),
                 }
             }
-            if cmd == "d" {
-                let number: usize = key.parse().expect("Not a valid number");
-                let result_delete = delete(number);
-                match result_delete {
-                    Ok(_) => println!("Delete password in position {}", key),
-                    Err(error) => {
-                        eprintln!("Error to delete password in position {} {}", key, error)
-                    }
-                }
-            }
-        }
-        _ => {
-            help();
-        }
+            None => eprintln!("Enter a password search string"),
+        },
     }
 }
